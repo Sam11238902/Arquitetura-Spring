07a3b5a4ff0af8c0bde00a2315b07988724c33d7
Samuel11238902 <samuel112928@gmail.com>
2024-11-06 13:51:13 -0300

    Inicializando o projeto de uma outra forma e adicionando dependencia h2

--- src/main/java/io/github/sam11238902/arquiteturaSpringBoot/ArquiteturaSpringBootApplication.java
+++ src/main/java/io/github/sam11238902/arquiteturaSpringBoot/ArquiteturaSpringBootApplication.java
@@ -2,11 +2,19 @@ package io.github.sam11238902.arquiteturaSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ArquiteturaSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArquiteturaSpringBootApplication.class, args);
+	//	SpringApplication.run(ArquiteturaSpringBootApplication.class, args);
+	
+		
+		
+		// faz a mesma coisa do codigo acima .
+		SpringApplicationBuilder build = new SpringApplicationBuilder(ArquiteturaSpringBootApplication.class);	
+		build.run(args);
+	
 	}
 }
