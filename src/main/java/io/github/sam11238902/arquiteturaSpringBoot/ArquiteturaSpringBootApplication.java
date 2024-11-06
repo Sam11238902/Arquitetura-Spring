aa9f3e8499f060067afb9e02dfbc7833dd6f2ee0
Samuel11238902 <samuel112928@gmail.com>
2024-11-06 13:21:25 -0300

    subindo o projeto local para o github

---
+++ src/main/java/io/github/sam11238902/arquiteturaSpringBoot/ArquiteturaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.sam11238902.arquiteturaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sam11238902/arquiteturaSpringBoot/ArquiteturaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sam11238902.arquiteturaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
