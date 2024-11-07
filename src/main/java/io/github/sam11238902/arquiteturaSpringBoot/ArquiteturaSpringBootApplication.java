4f717f9d84a3dbd7059a8a4ec3e3bb0b3175fbc7
Samuel11238902 <samuel112928@gmail.com>
2024-11-06 22:29:26 -0300

    Estudando a classe Aplication e o que se pode fazer com ela

--- src/main/java/io/github/sam11238902/arquiteturaSpringBoot/ArquiteturaSpringBootApplication.java
+++ src/main/java/io/github/sam11238902/arquiteturaSpringBoot/ArquiteturaSpringBootApplication.java
@@ -1,20 +1,42 @@
 package io.github.sam11238902.arquiteturaSpringBoot;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.Banner;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class ArquiteturaSpringBootApplication {
 
 	public static void main(String[] args) {
-	//	SpringApplication.run(ArquiteturaSpringBootApplication.class, args);
 	
+	
+		//SpringApplication.run(Application.class, args);
+
+		SpringApplicationBuilder builder =
+				new SpringApplicationBuilder(ArquiteturaSpringBootApplication.class);
+
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.profiles("producao", "homologacao");
+
+		builder.run(args);
+
+
+		// contexto da aplicação já iniciada:
+		ConfigurableApplicationContext applicationContext = builder.context();
+//		var produtoRepository = applicationContext.getBean("produtoRepository");
+
+
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		String applicationName = environment.getProperty("spring.application.name");
+		System.out.println("Nome da aplicação: " + applicationName);
+
+	}
 		
-		
-		// faz a mesma coisa do codigo acima .
-		SpringApplicationBuilder build = new SpringApplicationBuilder(ArquiteturaSpringBootApplication.class);	
-		build.run(args);
 	
 	}
-}
+
