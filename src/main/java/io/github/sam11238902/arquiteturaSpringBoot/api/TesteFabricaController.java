a4cf5b3bf0ca7701356fe5eb0820b26070aa015f
Samuel11238902 <samuel112928@gmail.com>
2024-11-07 13:40:27 -0300

    Adicionando @Qualifer para varios Beans da mesma entidade

--- src/main/java/io/github/sam11238902/arquiteturaSpringBoot/api/TesteFabricaController.java
+++ src/main/java/io/github/sam11238902/arquiteturaSpringBoot/api/TesteFabricaController.java
@@ -1,6 +1,7 @@
 package io.github.sam11238902.arquiteturaSpringBoot.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +17,30 @@ public class TesteFabricaController {
 
 	
 	
+	//QUALIFIER É UTILIZADO QUANDO EXISTE MAIS DE UM BEAN DO MESMO TIPO , DENTRO DO CONTAINER DE CONFIGURAÇÃO ,
+	//NOS DAMOS UM NOME AO BEAN COM O ATRIBUTO NAME , E UTILIZAMOS O QUALIFIER ABAIXO DA INJEÇAO COM O ATRIBUTO DO NOME DO BEAN . 
+	
 	@Autowired
-	private Motor motor;
+	@Qualifier(value = "motorAspirado")
+	private Motor motorAspirado;
+	
+	
+	
+	@Autowired
+	@Qualifier(value = "motorEletrico")
+	private Motor motorEletrico;
+	
+	
+	@Autowired
+	@Qualifier(value = "motorTurbo")
+	private Motor motorTurbo;
+	
+	
 	
 	
 	@PostMapping("/carros")
 	public CarroStatus ligarCarro(@RequestBody Chave chave) {
-		var carro = new HondaHRV(motor);
+		var carro = new HondaHRV(motorAspirado);
 		
 		
 		
--- src/main/java/io/github/sam11238902/arquiteturaSpringBoot/montadora/MontadoraConfiguration.java
+++ src/main/java/io/github/sam11238902/arquiteturaSpringBoot/montadora/MontadoraConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 public class MontadoraConfiguration {
 	
 	
-	@Bean
+	@Bean(name = "motorAspirado")
 	public Motor motor() {
 		var motor = new Motor();
 		motor.setCavalos(120);
@@ -15,6 +15,34 @@ public class MontadoraConfiguration {
 		motor.setModelo("XPT-9");
 		motor.setLitragem(2.0);
 		motor.setTipo(TipoMotor.ASPIRADO);
+		
+		
+		return motor;
+		
+	}
+	
+	@Bean(name = "motorEletrico")
+	public Motor motorEletrico() {
+		var motor = new Motor();
+		motor.setCavalos(120);
+		motor.setCilindros(4);
+		motor.setModelo("XPT-9");
+		motor.setLitragem(2.0);
+		motor.setTipo(TipoMotor.ELETRICO);
+		
+		
+		return motor;
+		
+	}
+	
+	@Bean(name = "motorTurbo")
+	public Motor motorTurbo() {
+		var motor = new Motor();
+		motor.setCavalos(120);
+		motor.setCilindros(4);
+		motor.setModelo("XPT-9");
+		motor.setLitragem(2.0);
+		motor.setTipo(TipoMotor.TURBO);
 		
 		
 		return motor;
